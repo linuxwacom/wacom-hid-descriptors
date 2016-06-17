bfbd509126607c1ba3f7e52d7bde300f48dfd8ff
Jason Gerecke <killertofu@gmail.com>
2016-06-17 14:47:07 -0700

    Update lsusb_to_hid.sh
    The earlier version of this script was unable to correctly handle lsusb
    output from multiple devices or devices with multiple interfaces. This
    rewrite tries to be more intelligent about processing one device at a
    time and processing all available report descriptors contained within.

--- scripts/lsusb_to_hid.sh
+++ scripts/lsusb_to_hid.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-OUTDIR=$(dirname "$1")
-OUTNAME=${OUTDIR}/descriptor-converted.hid
-echo $OUTDIR
-echo $OUTNAME
-awk '/Endpoint Descriptor/ { REPORT=0 } { if (length == 0) { REPORT = 0 } if (REPORT == 1) { print $0 } } /length is/ { REPORT=1 }' < "$1" | python2 lsusb_to_hex.py > "${OUTNAME}.bin"
-hidrd-convert -i natv -o spec "${OUTNAME}.bin" "${OUTNAME}.txt"
-hidrd-convert -i natv -o xml "${OUTNAME}.bin" "${OUTNAME}.xml"
+SRCFILE=$1
+OUTDIR=$(dirname "${SRCFILE}")
+RUNDIR=$(dirname "${0}")
+
+D=-1
+while [[ 1 ]]; do
+	D=$((D+1))
+	DATA=$(awk -vD=${D} 'BEGIN {d=-1;} /^Bus / {d++} {if (d==D) {print}}' "${SRCFILE}")
+	if [[ -z "${DATA}" ]]; then
+		break
+	fi
+
+	ID=$(echo "${DATA}" | head -n1 | awk '{print $6}')
+	NUM_IF=$(echo "${DATA}" | grep bNumInterfaces | head -n1 | awk '{print $2}')
+
+	for N in $(seq 0 ${NUM_IF}); do
+		OUTNAME="${OUTDIR}/${ID}_${N}.hid"
+		DESCRIPTOR=$(echo "${DATA}" | awk -vN=${N} 'BEGIN {L=9999} {l=match($0, /\S/)} /bInterfaceNumber/ {n=$2} /Report Descriptor/ {L=l; next} {if (n==N && l>L) {print} else {L=9999}}')
+
+		if [[ -z "${DESCRIPTOR}" ]]; then
+			continue
+		fi
+
+		echo "${DESCRIPTOR}" | python2 ${RUNDIR}/lsusb_to_hex.py > "${OUTNAME}.bin"
+		hidrd-convert -i natv -o spec "${OUTNAME}.bin" > "${OUTNAME}.txt"
+		hidrd-convert -i natv -o xml "${OUTNAME}.bin" > "${OUTNAME}.xml"
+	done
+done
