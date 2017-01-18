0759a2eec96dd71f9abd7dabc5a5fd2d6df5f3b6
Jason Gerecke <killertofu@gmail.com>
2017-01-18 08:23:40 -0800

    scripts: Improve functionality of sysinfo.sh script
    Multiple improvements to the sysinfo.sh script, including
    improved error handling and additional gathered data. In
    addition, it detects if it is run as root and gatheres additional
    data where possible (e.g. getting the HID descriptors on pre-3.17
    kernels requires us to temporarily unload the kernel driver, and
    some distros prevent us from using the udevadm command without
    root). As such, it now requests to be run as root, though it will
    still gather what data it can as a regular user.

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -1,39 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Gathering system & tablet information. This may take a few seconds."
+if [[ "$EUID" -ne 0 ]]; then echo "NOTE: It is recommended to run this tool as root."; fi
+
+echo "Gathering system and tablet information. This may take a few seconds."
 
 TMPDIR=$(mktemp -d --tmpdir sysinfo.XXXXXXXXXX) || { echo "Failed."; exit 1; }
+OUTFILE=$(readlink -m "$TMPDIR/../$(basename "$TMPDIR").tar.gz")
 cd "$TMPDIR"
 
-grep "" /sys/module/wacom/*version* > module_wacom.txt 2>&1
-modinfo wacom >> module_wacom.txt 2>&1
+function finish {
+	CODE=$1
+	cd ..
+	rm -rf "$TMPDIR"
+	exit $CODE
+}
+trap finish $? EXIT
 
-grep "" /sys/module/wacom_w8001/*version* > module_wacom_w8001.txt 2>&1
-modinfo wacom_w8001 >> module_wacom_w8001.txt 2>&1
 
-ls -l /usr/lib{,64}/libwacom.so* > userspace.txt 2>&1
-ls -l /usr/lib{,64}/libinput.so* >> userspace.txt 2>&1
-ls -l /usr/lib{,64}/xorg/modules/input/wacom_drv.so* >> userspace.txt 2>&1
-
-uname -a > host.txt 2>&1
+## General host information
+uname -a >> host.txt 2>&1
 HOST=$(lsb_release -a 2>/dev/null || hostnamectl 2>/dev/null || cat /etc/*release 2>/dev/null)
 echo "$HOST" >> host.txt
 
-lsusb -v -d 056a: > lsusb.txt 2>&1
-lsusb -v -d 0531: >> lsusb.txt 2>&1
-lsusb -t > lsusb_tree.txt 2>&1
+grep "" /sys/class/dmi/id/* >> machine.txt 2>&1
 
-grep "" /sys/class/dmi/id/* > sys_class_dmi_id.txt 2>&1
 
-for D in /sys/bus/hid/drivers/wacom/*; do
-	if test ! -f "$D/report_descriptor"; then
-		continue
+## Kernel driver information
+find /lib/modules/$(uname -r) -type f -iname "*wacom*" | xargs ls -l >> kernel_drivers.txt
+echo >> kernel_drivers.txt
+find /sys/module/ -type f -ipath "*wacom*/*version*" | xargs grep "" >> kernel_drivers.txt
+echo >> kernel_drivers.txt
+modinfo wacom >> kernel_drivers.txt 2>&1
+echo >> kernel_drivers.txt
+modinfo wacom_serial4 >> kernel_drivers.txt 2>&1
+echo >> kernel_drivers.txt
+modinfo wacom_w8001 >> kernel_drivers.txt 2>&1
+echo >> kernel_drivers.txt
+modinfo wacom_i2c >> kernel_drivers.txt 2>&1
+echo >> kernel_drivers.txt
+modinfo hid-wacom >> kernel_drivers.txt 2>&1
+
+
+## Kernel device information
+for DEV in /sys/module/*wacom*/drivers/*/*; do
+	if test -f "$DEV/report_descriptor"; then
+		cp "$DEV/report_descriptor" "$(basename "$DEV").hid.bin"
 	fi
 
-	cp "$D/report_descriptor" "$(basename "$D").hid.bin"
-	for X in "$D"/input/*/event*; do
-		 udevadm info -a -p "$X" > "udevadm_$(basename "$X").txt"
-	done
+	if test -d "$DEV/input"; then
+		for INPUT in $DEV/input/*; do
+			udevadm info -a -p "$INPUT" >> "udevadm_$(basename "$DEV").txt" 2>&1
+		done
+	fi
 done
 
 DO_PRINT=0
@@ -51,8 +69,53 @@ udevadm info -e | while read -r LINE; do
 	fi
 done
 
-cd ..
-tar czf "$(basename "$TMPDIR").tar.gz" "$(basename "$TMPDIR")"
-rm -rf "$TMPDIR"
+BINDLIST=""
+for D in /sys/module/wacom/drivers/usb:wacom/*; do
+	if test -d "$D/input"; then
+		# Temporarily unbind usb:wacom devices so lsusb
+		# can retrieve the HID descriptor
+		DEV=$(basename $D)
+		BINDLIST="$BINDLIST $DEV"
+		echo -n $DEV > /sys/module/wacom/drivers/usb:wacom/unbind
+	fi
+done
 
-echo "Finished. Data available in '$(pwd)/$(basename "$TMPDIR").tar.gz'"
+lsusb -v -d 056a: >> lsusb.txt 2>&1
+lsusb -v -d 0531: >> lsusb.txt 2>&1
+lsusb -v -d 2d1f: >> lsusb.txt 2>&1
+lsusb -t > lsusb_tree.txt 2>&1
+
+for DEV in $BINDLIST; do
+	# Rebind usb:wacom devices so that they function
+	# once again
+	echo -n $DEV > /sys/module/wacom/drivers/usb:wacom/bind
+done
+
+
+## Userspace driver information
+ls -l /usr/lib{,64}/xorg/modules/input/wacom_drv.so* \
+      /usr/lib{,64}/libwacom.so* \
+      /usr/lib{,64}/libinput.so* \
+      /usr/lib{,64}/xorg/modules/input/libinput_drv.so* \
+      >> userspace_drivers.txt 2>&1
+
+PACKAGES=$(pacman -Qi xf86-input-wacom libwacom libinput xf86-input-libinput 2>/dev/null || \
+           zypper info xf86-input-wacom libwacom libinput xf86-input-libinput 2>/dev/null || \
+           dpkg -s xserver-xorg-input-wacom libwacom2 libinput5 xserver-xorg-input-libinput 2>/dev/null || \
+           yum info xorg-x11-drv-wacom libwacom libinput xorg-x11-drv-libinput 2>/dev/null || \
+           dnf info xorg-x11-drv-wacom libwacom libinput xorg-x11-drv-libinput 2>/dev/null)
+echo "$PACKAGES" >> packages.txt
+
+xsetwacom -V >> xsetwacom.txt 2>&1
+
+
+## Userspace device information
+xinput list >> xinput.txt 2>&1
+xsetwacom -v list >> xsetwacom.txt 2>&1
+libwacom-list-local-devices >> libwacom.txt 2>&1
+libinput-list-devices >> libinput.txt 2>&1
+
+
+## Tarball generation
+tar czf "$OUTFILE" -C .. "$(basename "$TMPDIR")"
+echo "Finished. Data available in '$OUTFILE'"
